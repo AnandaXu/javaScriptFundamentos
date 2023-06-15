b63b81b95607c673143547f8ae7d033316ff183a
AnandaXu <anandarebella@gmail.com>
2023-06-15 22:53:17 +0200

    operadores e funções

--- 14-conversoes.js
+++ 14-conversoes.js
@@ -12,3 +12,38 @@ console.log( numero == numeroString);
 // função String ()
 
 console.log( numero + Number (numeroString));
+
+
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+if ( idadeCliente >= idadeMinima){
+    console.log ("cerveja")
+} else {
+    console.log("suco")
+};
+
+console.log( idadeCliente >= idadeMinima ? "cerveja" : "suco");
+
+
+//  RangeError: Quando o código recebe um dado do tipo certo, porém não dentro do formato aceitável. 
+//  Por exemplo, um processamento que só pode ser feito com números inteiros maiores ou iguais a zero, mas recebe -1.
+
+// ReferenceError: Normalmente ocorre quando o código tenta acessar algo que não existe, 
+// como uma variável que não foi definida; muitas vezes é causado por erros de digitação ou confusão nos nomes utilizados, 
+// mas também pode indicar um erro no programa.
+
+//  SyntaxError: Na maior parte dos casos ocorre quando há erros no programa e o JavaScript não 
+// consegue executá-lo. Os erros podem ser métodos ou propriedades escritos ou utilizados de forma incorreta, 
+// por exemplo, operadores ou sinais gráficos com elementos a menos, 
+// como esquecer de fechar chaves ou colchetes.
+
+ // TypeError: Indica que o código esperava receber um dado de um determinado tipo, tal qual uma string de 
+// texto, mas recebeu outro, como um número, booleano ou null.
+
+
+
+// ||: Operador “ou”, retorna true caso uma condição seja válida;
+// &&: Operador “e”, retorna true somente se todas as condições forem válidas;
+// != e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação, 
+// da mesma forma que == e === retornam true ou false.
---
+++ 15-funcao.js
@@ -0,0 +1,47 @@
+
+
+// declaração de função
+
+// function imprimeTexto (texto){
+//     console.log(texto)
+// }
+
+// executar a função uma ou mais vezes
+
+// imprimeTexto(soma());
+// imprimeTexto("xalalala"); // com a mesma função se imprime outro código
+
+// // 3 formas de escrever funções
+
+// function soma (){
+//     return 2 + 2;
+// }
+
+//console.log(soma());
+
+
+
+// PARAMETROS DE FUNÇÃO
+
+// function soma (numero1, numero2){
+//     return numero1 + numero2;
+// }
+
+// console.log(soma(2 , 2));
+// console.log(soma( 55, 20 ));
+// console.log(soma( -300, 25)); // VAI FAZER A CONTA PELA ORDEM DECLARADA
+
+
+//////////////////////////////
+
+// function minhaFuncao(param){
+
+// }
+
+// minhaFuncao(param);
+
+//expressão de função
+
+const soma = function ( num1, num2) { return num1 + num2}
+
+console.log(soma (1,3));
