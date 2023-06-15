0c4f6f61dbc03507c2430edfbdd47843ead67f13
AnandaXu <106769176+AnandaXu@users.noreply.github.com>
2023-06-15 18:58:33 +0200

    Add files via upload

---
+++ 14-conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dados 
+// booleanos
+
+//conversao implicita; === comparação direta, == ele converte ambos em texto e vai dar true
+
+const numero = 456;
+const numeroString = "456";
+
+console.log( numero == numeroString);
+
+//função Number () isso pode ser passado dentro da variavel, sempre fechando o numero com parenteses
+// função String ()
+
+console.log( numero + Number (numeroString));
